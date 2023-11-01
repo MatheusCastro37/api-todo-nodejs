bedc3d59b784b6635ebca1756a0f1f3d2b92b8af
Matheus Castro <castro.dev37@gmail.com>
2023-11-01 19:38:03 -0300

    chore: modificando acesso a variaveis ambiente.

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 import postgres from 'postgres';
 import 'dotenv/config';
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-
 export const sql = postgres({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
   port: 5432,
   ssl: 'require',
   connection: {
-    options: `project=${ENDPOINT_ID}`,
+    options: `project=${process.env.ENDPOINT_ID}`,
   },
 });
