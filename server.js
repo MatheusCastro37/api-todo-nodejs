d5b479703452a60c49eaea464c4cc00bcf9dd319
Matheus Castro <castro.dev37@gmail.com>
2023-11-02 10:27:41 -0300

    feat: criando rota de POST.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-import { sql } from './app.js';
 import Fastify from 'fastify';
+
+import { sql } from './app.js';
+
+import { v4 as uuidv4 } from 'uuid';
 
 const fastify = Fastify({
     logger: true
@@ -10,6 +13,14 @@ const port = 3333;
 // Declare a route
 fastify.get('/', async function() {
     return await sql`SELECT * FROM user_info`
+})
+
+fastify.post('/createUser', async function(req, res) {
+    const userID = uuidv4();
+    await sql`
+    INSERT INTO user_info(user_id,user_name,user_email,user_password)
+    VALUES(${userID},${req.body.name},${req.body.email},${req.body.password})
+    `
 })
 
 // Run the server!
