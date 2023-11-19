759bc649e6f2420daf7afe6c370a7210bff06464
Matheus Castro <castro.dev37@gmail.com>
2023-11-19 14:09:10 -0300

    feat: incrementando criação de usuario.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 
+import bcrypt from 'bcrypt';
+
 import { sql } from './app.js';
 
 import { v4 as uuidv4 } from 'uuid';
@@ -29,17 +31,29 @@ fastify.post('/', async function(req, res) {
 
 })
 
-fastify.get('/todoList/:user_id', async function(req, res) {
-    const { user_id } = req.params
-    return await sql`SELECT * FROM user_todos WHERE user_id_todo = ${user_id}`
-})
-
 fastify.post('/createUser', async function(req, res) {
+
+    var { name, email, password } = req.body
+
+    var passwordString = password.toString();
+    
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(passwordString, salt)
+
     const userID = uuidv4();
-    await sql`
-    INSERT INTO user_info(user_id,user_name,user_email,user_password)
-    VALUES(${userID},${req.body.name},${req.body.email},${req.body.password})
-    `
+
+    try {
+        
+        await sql`
+            INSERT INTO user_info(user_id,user_name,user_email,user_password)
+            VALUES(${userID},${name},${email},${passwordHash})
+        `
+        res.status(201).send({ msg: "usuario criado com sucesso!" })
+        
+    } catch {
+        res.status(400).send({ msg: "Usuario ja existente!"})
+    }
+    
 })
 
 fastify.post('/todoList/:user_id', async function(req, res) {
@@ -49,6 +63,11 @@ fastify.post('/todoList/:user_id', async function(req, res) {
         INSERT INTO user_todos(todo_id, todo_name, user_id_todo)
         VALUES(${todoID}, ${req.body.todo},${user_id})
     `
+})
+
+fastify.get('/todoList/:user_id', async function(req, res) {
+    const { user_id } = req.params
+    return await sql`SELECT * FROM user_todos WHERE user_id_todo = ${user_id}`
 })
 
 // Run the server!
