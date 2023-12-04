0e3876134efc739ec5a6926d8678e9c32a4cce5f
Matheus Castro <castro.dev37@gmail.com>
2023-12-04 15:42:16 -0300

    refactor: dominio vercel.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ fastify.post('/', async function(req, res) {
         const token = Jwt.sign({ userID: verifyUserDB.user_id, email: verifyUserDB.user_email }, process.env.SECRET)
 
         res.cookie('tokenAPI', token, {
-            domain: 'api-todo-nodejs.onrender.com',
+            domain: 'todo-list-beta-sable.vercel.app',
             httpOnly: true,
             signed: true,
             secure: true,
