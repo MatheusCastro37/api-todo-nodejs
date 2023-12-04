e1634312372937024f160ddbcd0aa9dd15aad06c
Matheus Castro <castro.dev37@gmail.com>
2023-12-04 15:21:09 -0300

    feat: dominio do render.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ fastify.post('/', async function(req, res) {
         const token = Jwt.sign({ userID: verifyUserDB.user_id, email: verifyUserDB.user_email }, process.env.SECRET)
 
         res.cookie('tokenAPI', token, {
-            domain: 'https://todo-list-beta-sable.vercel.app',
+            domain: 'https://api-todo-nodejs.onrender.com',
             httpOnly: true,
             signed: true,
             secure: true,
