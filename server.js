fe4d8c7e87d502af393aa90ffa2c88e1b435efd4
Matheus Castro <castro.dev37@gmail.com>
2023-12-04 14:29:54 -0300

    feat: partitioned.

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ fastify.post('/', async function(req, res) {
             secure: true,
             sameSite: 'none',
             path: '/',
+            partitioned: true,
             maxAge: 123456789
         })
 
