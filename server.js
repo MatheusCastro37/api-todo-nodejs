7e7dc3007038af9ce01ccbe20ccbc6928dd9d3de
Matheus Castro <castro.dev37@gmail.com>
2023-12-04 15:59:56 -0300

    Revert "refactor: retirando dominio."
    This reverts commit 667fbd55935e23a6d79dee85a50a623ecfab572a.

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ fastify.post('/', async function(req, res) {
         const token = Jwt.sign({ userID: verifyUserDB.user_id, email: verifyUserDB.user_email }, process.env.SECRET)
 
         res.cookie('tokenAPI', token, {
+            domain: 'api-todo-nodejs.onrender.com',
             httpOnly: true,
             signed: true,
             secure: true,
