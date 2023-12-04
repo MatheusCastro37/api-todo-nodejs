7a3de2d28fc293ee1ecb40a4949794e8c44d78bd
Matheus Castro <castro.dev37@gmail.com>
2023-12-04 14:18:45 -0300

    refactor: dominio do cookie.

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ fastify.post('/', async function(req, res) {
         const token = Jwt.sign({ userID: verifyUserDB.user_id, email: verifyUserDB.user_email }, process.env.SECRET)
 
         res.cookie('tokenAPI', token, {
-            domain: 'https://todo-list-beta-sable.vercel.app',
             httpOnly: true,
             signed: true,
             secure: true,
