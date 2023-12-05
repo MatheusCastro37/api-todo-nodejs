fe886e37495c40565340134a882bc340e5f460e0
Matheus Castro <castro.dev37@gmail.com>
2023-12-05 11:48:37 -0300

    Revert "refactor: dominio."
    This reverts commit 9fac0603815331d86761598f82544f05fc86e0dc.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ fastify.post('/', async function(req, res) {
         const token = Jwt.sign({ userID: verifyUserDB.user_id, email: verifyUserDB.user_email }, process.env.SECRET)
 
         res.setCookie('tokenAPI', token, {
-            domain: 'todo-list-gold-three.vercel.app',
+            domain: 'api-todo-nodejs.onrender.com',
             httpOnly: true,
             signed: true,
             secure: true,
